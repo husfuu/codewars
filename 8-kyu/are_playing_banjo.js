00cf84a54c777e4e360ab3739c31d0359ba306bc
husfuu <muhammadhusninurfadillah@gmail.com>
2024-04-12 04:14:42 +0700

    feat(8-kyu): Are You Playing Banjo?

---
+++ 8-kyu/are_playing_banjo.js
@@ -0,0 +1,21 @@
+function areYouPlayingBanjo(name) {
+    if (name[0] == "R" || name[0] == "r") {
+      return `${name} plays banjo`
+    }
+    
+    return `${name} does not play banjo`;
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+    assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+    })
+  })
